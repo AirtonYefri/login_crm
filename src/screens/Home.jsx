51fc5c3d595dbbf275ac9c9d0287e4d0f63ed214
JLeo-83 <143038770+JLeo-83@users.noreply.github.com>
2023-11-15 21:45:45 -0500

    flitros de para chats

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,33 +1,84 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; // Asegúrate de que la importación sea correcta
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomNavigationBar from '../navigator/NavigationChatCRM.jsx';
 import HeaderChatCrm from '../header/HeaderChatCrm.jsx';
 import { styles } from '../style/styles.jsx';
 
+// Componente ChatScreen
 function ChatScreen() {
+  const [selectedGroup, setSelectedGroup] = useState(null);
   const [activeTab, setActiveTab] = useState(0);
   const [searchText, setSearchText] = useState('');
+  const [chats, setChats] = useState([
+    { id: 1, content: 'Mensaje 1', group: 'Familia', hasResponse: true },
+    { id: 2, content: 'Mensaje 2', group: 'Trabajo', hasResponse: false },
+    { id: 3, content: 'Mensaje 3', group: 'Amigos', hasResponse: true },
+    // ... otros chats
+  ]);
 
+  
+
+  // Funciones lógicas
   const handleSearch = () => {
-    // Aquí puedes agregar la lógica para manejar la búsqueda
+    // Lógica para manejar la búsqueda
     console.log('Búsqueda:', searchText);
   };
 
   const handleTabPress = (tabId) => {
     setActiveTab(tabId);
-    // Aquí puedes realizar acciones específicas cuando se cambia la pestaña
+    // Puedes realizar acciones específicas cuando se cambia la pestaña
   };
 
+  const handleSelectGroup = () => {
+    // Lógica para manejar la selección de grupo
+    Alert.alert(
+      'Seleccionar Grupo',
+      null,
+      availableGroups.map((group) => ({
+        text: group,
+        onPress: () => {
+          setSelectedGroup(group);
+        },
+      })),
+    );
+  };
+
+  // Función de filtrado
+const filteredChats = () => {
+  switch (activeTab) {
+    case 0: // Todos
+      return chats;
+    case 1: // Sin responder
+      return chats.filter((chat) => !chat.hasResponse);
+    case 2: // Grupos
+      return chats.filter((chat) => chat.group === 'Amigos'); // Cambia 'Amigos' por el grupo deseado
+    default:
+      return chats;
+  }
+};
+
+
+  // Renderización
   return (
     <View style={{ flex: 1 }}>
       <HeaderChatCrm />
-      <View style={{ marginTop: 15 }} >
-        {/* Asumiendo que CustomNavigationBar está definido en 'NavigationChatCRM.jsx' */}
-        <CustomNavigationBar activeTab={activeTab} onTabPress={handleTabPress} />
+      <View style={{ marginTop: 15 }}>
+        <CustomNavigationBar activeTab={activeTab} onTabPress={handleTabPress} onSelectGroup={handleSelectGroup} />
       </View>
-      {/* Resto del contenido de la pantalla */}
-      <Text>Contenido de la pestaña {activeTab}</Text>
+      <TextInput
+        placeholder="Buscar mensajes"
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+        onBlur={handleSearch}
+      />
+      {selectedGroup && (
+        <Text>Chats del grupo: {selectedGroup}</Text>
+      )}
+      {filteredChats().map((chat) => (
+        <Text key={chat.id}>{chat.content}</Text>
+      ))}
     </View>
   );
 }
